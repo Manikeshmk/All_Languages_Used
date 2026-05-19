b9b4f35e98a601842eefad9c096f1a90dcc8c429
Manikesh Kumar <23bds032@iiitdwd.ac.in>
2026-05-19 15:39:57 +0530

    Update languages-json.ts

--- api/languages-json.ts
+++ api/languages-json.ts
@@ -86,7 +86,11 @@ export default async function handler(
     };
 
     // Store in cache
-    (cache as any).set(cacheKey,userData,getCacheTTL());
+    (cache as any)["set"](
+  cacheKey,
+  userData,
+  getCacheTTL()
+);
 
     // Headers
     res.setHeader('Content-Type', 'application/json');
